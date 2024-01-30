2c930f5eadf862563b7166dcbf3bbadeeac2d6db
Ji Park <ji.park@jpnws.com>
2024-01-29 20:38:22 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
+const headerBox = document.querySelector('.header-box');
+
+navToggle.addEventListener('click', () => {
+  nav.classList.toggle('nav-visible');
+  headerBox.classList.toggle('header-box-visible');
+});
